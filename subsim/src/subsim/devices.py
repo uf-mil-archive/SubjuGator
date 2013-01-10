ddbd4dbace6a8d82438a16af172220c84806fa7e
Forrest Voight <forrest@forre.st>
2013-01-10 11:21:45 -0500

    added subsim with launch file that gets everything running

---
+++ subsim/src/subsim/devices.py
@@ -0,0 +1,328 @@
+from __future__ import division
+
+import itertools
+import math
+import random
+import struct
+
+from twisted.internet import protocol, task
+from twisted.python import log
+
+import datachunker
+
+perfect_round = lambda x: (-1 if x < 0 else 1)*int(abs(x) + random.random())
+
+def crc16(data, prev=0, mask=0x1021):
+    crc = prev
+    for char in data:
+        c = ord(char)
+        c = c << 8
+        
+        for j in xrange(8):
+            if (crc ^ c) & 0x8000:
+                crc = ((crc << 1) ^ mask) & 0xffff
+            else:
+                crc = (crc << 1) & 0xffff
+            c = c << 1
+    return crc
+
+class EmbeddedProtocol(protocol.Protocol):
+    def connectionMade(self):
+        self.dataReceived = datachunker.DataChunker(self.dataReceiver())
+        self.packet_count = 0
+    
+    def datagramReceived(self, data, addr):
+        self.dataReceived(data)
+    
+    def dataReceiver(self):
+        while True:
+            # wait for 7E
+            garbage = []
+            while True:
+                b = yield 1
+                if b == '\x7e':
+                    break
+                garbage.append(b)
+            if garbage:
+                print self, 'embedded garbage', ''.join(garbage).encode('hex')
+            
+            data = []
+            while True:
+                b = yield 1
+                if b == '\x7e' and not data: # ignore leading 7E - zero length message will never happen, so we're out of sync
+                    pass
+                elif b == '\x7e' and data:
+                    break
+                elif b == '\x7d':
+                    data.append(chr(ord((yield 1)) ^ 0x20))
+                else:
+                    data.append(b)
+            data2 = ''.join(data)
+            
+            #print data2.encode('hex')
+            
+            if len(data2) < 2 or data2[-2:] != struct.pack('<H', crc16(data2[:-2])):
+                print 'embedded invalid', data.encode('hex')
+                continue
+            data3 = data2[:-2]
+            
+            try:
+                (dest_address, src_address, packetcount), data4 = struct.unpack('BBH', data3[:4]), data3[4:]
+                
+                if dest_address != self.local_address or src_address != self.remote_address:
+                    print 'embedded address mismatch', (dest_address, src_address), (self.local_address, self.remote_address)
+                    continue
+                
+                if data4:
+                    (typecode,), data5 = struct.unpack('B', data4[:1]), data4[1:]
+                else:
+                    typecode, data5 = None, ''
+                
+                self.packetReceived(typecode, data5)
+            except:
+                print
+                print self, "Error handling packet:", data3.encode('hex')
+                log.err()
+                print
+    
+    def sendPacket(self, typecode, contents):
+        data_pre = struct.pack('BBHB', self.remote_address, self.local_address, self.packet_count % 2**16, typecode) + contents
+        data = data_pre + struct.pack('H', crc16(data_pre))
+        
+        res = []
+        res.append('\x7e')
+        for char in data:
+            if char == '\x7d' or char == '\x7e':
+                res.append('\x7d')
+                res.append(chr(ord(char) ^ 0x20))
+            else:
+                res.append(char)
+        res.append('\x7e')
+        self.transport.write(''.join(res))
+        
+        self.packet_count += 1
+
+class ThrusterProtocol(EmbeddedProtocol):
+    local_address = 21
+    # remote_address set in __init__
+    
+    def __init__(self, thruster_id, thrusters):
+        self.thruster_id = thruster_id
+        self.thrusters = thrusters
+        
+        self.remote_address = 30 + thruster_id
+        
+        self.update_task = task.LoopingCall(self.sendUpdate)
+    
+    def connectionMade(self):
+        EmbeddedProtocol.connectionMade(self)
+        print 'thruster', self.thruster_id, 'connection made'
+        self.update_task.start(.1)
+    
+    def sendUpdate(self):
+        tickcount = 0
+        flags = 1 # valid motor
+        refinput = self.thrusters[self.thruster_id] # ???
+        presentoutput = 0
+        railvoltage = 0
+        current = 0
+        data = struct.pack('HHHHHH',
+            tickcount,
+            flags,
+            (lambda x: 0x8000|-x if not 0 <= x < 0x8000 else x)(perfect_round(refinput * 2**8)),
+            perfect_round(presentoutput * 2**10),
+            perfect_round(railvoltage * 2**10),
+            perfect_round(current * 2**12),
+        )
+        assert len(data) == 12
+        #self.sendPacket(0, data)
+    
+    def packetReceived(self, typecode, data):
+        if typecode == 0 and data[0] == '\x03':
+            x, = struct.unpack('<H', data[1:])
+            if x & 0x8000:
+                x = x ^ 0x8000
+            else:
+                x = -x
+            x = x/100/2**8
+            #print self.thruster_id, data.encode('hex')
+            self.thrusters[self.thruster_id] = x
+        else:
+            print 'thruster', self.thruster_id, typecode, data.encode('hex')
+    
+    def connectionLost(self, reason):
+        self.update_task.stop()
+        print 'thruster', self.thruster_id, 'connection lost', reason
+
+class IMUProtocol(protocol.Protocol):
+    def __init__(self, listener_set):
+        self.listener_set = listener_set
+    
+    def connectionMade(self):
+        print 'imu connection made'
+        self.listener_set.add(self)
+    
+    def dataReceived(self, data):
+        print 'imu', data.encode('hex')
+    
+    def sendUpdate(self, flags, supply_voltage, ang_rate, acceleration, mag_field, temperature, timestamp):
+        #print "M", mag_field
+        #print 
+        #print acceleration
+        #print [perfect_round(x/9.80665/0.00333) for x in acceleration]
+        data = ''.join([
+            struct.pack('<HH', flags, perfect_round(supply_voltage/0.00242)),
+            struct.pack('<3h', *(perfect_round(x/0.000872664626) for x in ang_rate)),
+            struct.pack('<3h', *(perfect_round(x/9.80665/0.00333) for x in acceleration)),
+            struct.pack('<3h', *(perfect_round(x/0.0005) for x in mag_field)),
+            struct.pack('<hq', perfect_round((temperature - 25)/0.14), perfect_round(timestamp*1e9)),
+        ])
+        assert len(data) == 32
+        self.transport.write(data)
+    
+    def connectionLost(self, reason):
+        self.listener_set.remove(self)
+        print 'imu connection lost'
+
+class DepthProtocol(EmbeddedProtocol):
+    local_address = 21
+    remote_address = 40
+    
+    def __init__(self, listener_set):
+        self.listener_set = listener_set
+    
+    def connectionMade(self):
+        EmbeddedProtocol.connectionMade(self)
+        print 'depth connection made'
+        self.listener_set.add(self)
+    
+    def packetReceived(self, typecode, data):
+        if typecode == 100:
+            assert not data, data.encode('hex')
+        else:
+            print 'depth', typecode, data.encode('hex')
+    
+    def sendUpdate(self, tickcount, flags, depth, thermister_temp, humidity, humidity_sensor_temp):
+        depth = depth/1.45 + 10.62
+        data = struct.pack('<HBHHHH',
+            tickcount, flags, perfect_round(depth*2**10), perfect_round(thermister_temp*2**8),
+            perfect_round(humidity), perfect_round(humidity_sensor_temp))
+        assert len(data) == 11
+        self.sendPacket(4, data)
+        #print repr(data)
+    
+    def connectionLost(self, reason):
+        self.listener_set.remove(self)
+
+class ActuatorProtocol(protocol.Protocol):
+    def __init__(self):
+        self.update_task = task.LoopingCall(self.sendUpdate)
+    
+    def connectionMade(self):
+        print 'actuator connection made'
+        self.update_task.start(.1)
+    
+    def dataReceived(self, data):
+        for byte in data:
+            self.packetReceived(ord(byte))
+    
+    def packetReceived(self, data):
+        print 'actuator', data
+    
+    def sendUpdate(self):
+        self.transport.write('\x00')
+    
+    def connectionLost(self, reason):
+        self.update_task.stop()
+        print 'actuator connection lost'
+
+class DVLProtocol(protocol.Protocol):
+    def __init__(self, listener_set):
+        self.listener_set = listener_set
+    
+    def connectionMade(self):
+        print 'dvl connection made'
+        self.listener_set.add(self)
+        self.transport.write('Teledyne RD Instruments (c) 2007')
+    
+    def dataReceived(self, data):
+        print 'dvl', repr(data)
+    
+    def sendHighResVel(self, bottom_vel, bottom_dist, water_vel, water_dist, speed_of_sound):
+        if not all(len(x) == 4 for x in [bottom_vel, bottom_dist, water_vel, water_dist]):
+            raise TypeError('first four arguments must each have a length of four')
+        self.sendUpdate(['\x80\x00' + '\x01'*58] + [''.join([
+            '\x03\x58',
+            struct.pack('16i', *(perfect_round(x*1e5) for x in itertools.chain(bottom_vel, bottom_dist, water_vel, water_dist))),
+            struct.pack('i', perfect_round(speed_of_sound*1e6)),
+        ])] + ['\x00\x06' + '\x00'*79] + ['\x04\x58' + '\x00'*37])
+    
+    def sendUpdate(self, msgs):
+        header_length = 1 + 1 + 2 + 1 + 1 + 2 * len(msgs)
+        data = ''.join([
+            '\x7f\x7f',
+            struct.pack('<HBB', header_length + sum(map(len, msgs)) + 2, random.randrange(2**8), len(msgs)),
+            ''.join(struct.pack('<H', header_length + sum(map(len, msgs[:i]))) for i in xrange(len(msgs))),
+            ''.join(msgs),
+            struct.pack('<H', random.randrange(2**16)),
+        ])
+        data_with_checksum = data + struct.pack('<H', sum(map(ord, data)))
+        self.transport.write(data_with_checksum)
+        #print data_with_checksum.encode('hex')
+    
+    def connectionLost(self, reason):
+        self.listener_set.remove(self)
+        print 'dvl connection lost'
+
+class HydrophoneProtocol(protocol.Protocol):
+    pass
+
+class MergeProtocol(EmbeddedProtocol):
+    local_address = 21
+    remote_address = 60
+    
+    def __init__(self, listener_set):
+        self.listener_set = listener_set
+    
+    def connectionMade(self):
+        EmbeddedProtocol.connectionMade(self)
+        print 'merge connection made'
+        self.listener_set.add(self)
+    
+    def packetReceived(self, typecode, data):
+        print "merge", typecode, data.encode('hex')
+    
+    def sendUpdate(self, tickcount, flags, current16, voltage16, current32, voltage32):
+        data = struct.pack('<HBHHHH', tickcount, flags,
+            perfect_round(current16 * 2**10),
+            perfect_round(voltage16 * 2**10),
+            perfect_round(current32 * 2**10),
+            perfect_round(voltage32 * 2**10),
+        )
+        assert len(data) == 11
+        self.sendPacket(6, data)
+    
+    def connectionLost(self, reason):
+        self.listener_set.remove(self)
+        print 'merge connection lost'
+
+
+class HeartbeatProtocol(EmbeddedProtocol):
+    local_address = 21
+    remote_address = 255
+    
+    def connectionMade(self):
+        EmbeddedProtocol.connectionMade(self)
+        print 'heartbeat connection made'
+    
+    def packetReceived(self, typecode, data):
+        if typecode is None:
+            return # XXX??
+        if typecode == 100:
+            assert not data
+        else:
+            print "heartbeat", typecode, data.encode('hex')
+    
+    def connectionLost(self, reason):
+        print 'heartbeat connection lost'
+
---
+++ subsim/src/subsim/threed.py
@@ -0,0 +1,457 @@
+from __future__ import division
+
+import math
+import sys
+import random
+
+import numpy
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import pygame
+
+import roslib
+roslib.load_manifest('sensor_msgs')
+roslib.load_manifest('tf')
+import rospy
+import tf
+from tf import transformations
+
+from sensor_msgs.msg import Image, CameraInfo
+
+from vector import v, V
+
+@apply
+class GLMatrix(object):
+    def __enter__(self):
+        glPushMatrix()
+    
+    def __exit__(self, type, value, traceback):
+        glPopMatrix()
+
+class DisplayList(object):
+    def __init__(self):
+        self.sl = glGenLists(1)
+    
+    def __del__(self):
+        glDeleteLists(self.sl, 1)
+    
+    def __enter__(self):
+        glNewList(self.sl, GL_COMPILE)
+    
+    def __exit__(self, type, value, traceback):
+        glEndList()
+    
+    def __call__(self):
+        glCallList(self.sl)
+
+def display_list(callable):
+    cache = {}
+    def x(*args):
+        if args not in cache:
+            dl = DisplayList()
+            with dl:
+                callable(*args)
+            cache[args] = dl
+        cache[args]()
+    return x
+
+def angleaxis_matrix(angle, (x, y, z)):
+    s = math.sin(angle)
+    c = math.cos(angle)
+    return numpy.array([
+        [(1-c)*x*x + c,   (1-c)*y*x - z*s, (1-c)*z*x + y*s, 0],
+        [(1-c)*x*y + z*s, (1-c)*y*y + c,   (1-c)*z*y - x*s, 0],
+        [(1-c)*x*z - y*s, (1-c)*y*z + x*s, (1-c)*z*z + c,   0],
+        [0,               0,                0,              1],
+    ])
+
+def euler_matrix(yaw, pitch, roll):
+    return numpy.dot(angleaxis_matrix(roll, (1, 0, 0)), numpy.dot(angleaxis_matrix(pitch, (0, 1, 0)), angleaxis_matrix(yaw, (0, 0, 1))))
+
+def rotate_vec(vec, m):
+    x = numpy.dot((vec[0], vec[1], vec[2], 1), m)
+    return V(x[:3])/x[3]
+
+def mesh_from_obj(file):
+    vertices = []
+    texcoords = []
+    normals = []
+    indices = []
+    for line in file:
+        line = line.strip()
+        if '#' in line:
+            line = line[:line.index("#")]
+        if not line: continue
+        line = line.split(' ')
+        if line[0] == "v":
+            vertices.append(V(float(x) for x in line[1:]))
+        if line[0] == "vt":
+            texcoords.append(V(float(x) for x in line[1:]))
+        if line[0] == "vn":
+            normals.append(V(float(x) for x in line[1:]))
+        elif line[0] == "f":
+            indices.append(tuple(tuple(int(y)-1 if y else None for y in x.split('/')) for x in line[1:]))
+    return Mesh(vertices, texcoords, normals, indices)
+
+class Mesh(object):
+    def __init__(self, vertices, texcoords, normals, indices):
+        self.vertices = vertices
+        self.texcoords = texcoords
+        self.normals = normals
+        self.indices = indices
+    
+    @display_list
+    def draw(self):
+        glBegin(GL_TRIANGLES)
+        for triangle in self.indices:
+            for vert_index, tex_index, normal_index in triangle:
+                if tex_index is not None:
+                    glTexCoord2f(*self.texcoords[tex_index])
+                if normal_index is not None:
+                    glNormal3f(*self.normals[normal_index])
+                glVertex3f(*self.vertices[vert_index])
+        glEnd()
+    
+    @property
+    def ode_trimeshdata(self):
+        import ode
+        x = ode.TriMeshData()
+        x.build(self.vertices, [[p[0] for p in t] for t in self.indices])
+        return x
+
+
+def rotate_to_body(body, inv=False):
+    R = body.getRotation()
+    p = body.getPosition()
+    rot = [[R[0], R[3], R[6], 0.],
+        [R[1], R[4], R[7], 0.],
+        [R[2], R[5], R[8], 0.],
+        [p[0], p[1], p[2], 1.]]
+    if inv:
+        rot = numpy.linalg.inv(rot)
+    rot = list(rot[0])+list(rot[1])+list(rot[2])+list(rot[3])
+    glMultMatrixd(rot)
+
+class Sub(object):
+    def __init__(self, body):
+        self.body = body
+        self.vectors = []
+    
+    def draw(self):
+        glBegin(GL_LINES)
+        glColor3f(1, 1, 1)
+        glVertex3f(*self.body.getPosition())
+        glVertex3f(*self.body.getPosition() + 20*v(0.241510, -0.022442, 0.405728))
+        glEnd()
+        with GLMatrix:
+            rotate_to_body(self.body)
+            
+            q = gluNewQuadric()
+            with GLMatrix:
+                glTranslate(+.2, 0, 0)
+                glColor3f(0, 1, 0)
+                gluSphere(q, 0.5, 40, 20)
+            with GLMatrix:
+                glTranslate(-.2, 0, 0)
+                glColor3f(1, 0, 0)
+                gluSphere(q, 0.5, 40, 20)
+            with GLMatrix:
+                glTranslate(+.4, +.2, -.3)
+                glColor3f(0, 0, 0)
+                gluSphere(q, 0.1, 40, 20)
+            with GLMatrix:
+                glTranslate(+.4, -.2, -.3)
+                glColor3f(0, 0, 0)
+                gluSphere(q, 0.1, 40, 20)
+            
+            glDisable(GL_DEPTH_TEST)
+            glBegin(GL_LINES)
+            for start, end in self.vectors:
+                glColor3f(0, 0, 0)
+                glVertex3f(*start)
+                glColor3f(1, 1, 1)
+                glVertex3f(*end)
+            glEnd()
+            glEnable(GL_DEPTH_TEST)
+
+class Buoys(object):
+    def __init__(self, pos):
+        self.buoys = [(pos + [random.gauss(0, 1) for i in xrange(3)], color) for color in [(1, 0, 0), (0, 1, 0), (1, 1, 0)]]
+    
+    def draw(self):
+        q = gluNewQuadric()
+        for pos, color in self.buoys:
+            with GLMatrix:
+                glTranslate(*pos)
+                glColor3f(*color)
+                gluSphere(q, .2, 30, 15)
+
+class MeshDrawer(object):
+    def __init__(self, mesh, color):
+        self.mesh = mesh
+        self.color = color
+    
+    def draw(self):
+        glColor3f(*self.color)
+        self.mesh.draw()
+
+class VectorField(object):
+    def __init__(self, func):
+        self.func = func
+    
+    def draw(self):
+        glBegin(GL_LINES)
+        for x in xrange(-10, 10+1):
+            for y in xrange(-10, 10+1):
+                pos = v(x, y, 0)
+                vel = self.func(pos)
+                glColor3d(1, 0, 0)
+                glVertex3f(*pos-vel/2)
+                glColor3d(1, 1, 0)
+                glVertex3f(*pos+vel/2)
+        glEnd()
+
+def perspective(fovy, aspect, zNear):
+    f = 1/math.tan(math.radians(fovy)/2)
+    glMultMatrixf([
+        [f/aspect, 0, 0, 0],
+        [ 0, f, 0, 0],
+        [0, 0, -1, -1],
+        [0, 0, -2*zNear, 0]
+    ])
+
+class Interface(object):
+    def init(self):
+        self.display_flags = pygame.DOUBLEBUF|pygame.OPENGL|pygame.RESIZABLE
+        self.display = pygame.display.set_mode((700, 400), self.display_flags)
+        self.clock = pygame.time.Clock()
+        
+        self.pitch = self.yaw = self.roll = 0
+        self.grabbed = False
+        
+        self.fovy = 100
+        
+        self.t = 0
+        self.pos = v(-10, 0, -2)
+        
+        self.objs = []
+        self.objs.append(Buoys(v(10, 3, 4)))
+        
+        rospy.init_node('sim')
+        self.image_pub = rospy.Publisher('image_rect_color', Image)
+        self.info_pub = rospy.Publisher('camera_info', CameraInfo)
+        self.tf_br = tf.TransformBroadcaster()
+    
+    def step(self):
+        dt = self.clock.tick()/1000
+        self.t += dt
+        
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                if self.grabbed:
+                    self.yaw += -event.rel[0]/100
+                    
+                    self.pitch += event.rel[1]/100
+                    # caps it to a quarter turn up or down
+                    self.pitch = min(max(self.pitch, -math.pi/2), math.pi/2)
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                    self.grabbed = not self.grabbed
+                    pygame.event.set_grab(self.grabbed)
+                    pygame.mouse.set_visible(not self.grabbed)
+                
+                elif event.key == pygame.K_q:
+                    sys.exit()
+            
+            elif event.type == pygame.QUIT:
+                sys.exit()
+            
+            elif event.type == pygame.VIDEORESIZE:
+                self.display = pygame.display.set_mode(event.size, self.display_flags)
+        
+        rot_matrix = euler_matrix(self.yaw, self.pitch, self.roll)
+        
+        forward = rotate_vec(v(1,0,0), rot_matrix)
+        left = rotate_vec(v(0,-1,0), rot_matrix)
+        local_up = rotate_vec(v(0,0,-1), rot_matrix)
+        
+        keys = pygame.key.get_pressed()
+        
+        speed = 50 if keys[pygame.K_LSHIFT] else 5
+        speed *= (.1 if keys[pygame.K_LCTRL] else 1)
+        
+        if keys[pygame.K_w]: self.pos += forward*dt*speed
+        if keys[pygame.K_s]: self.pos += -forward*dt*speed
+        if keys[pygame.K_a]: self.pos += left*dt*speed
+        if keys[pygame.K_d]: self.pos += -left*dt*speed
+        if keys[pygame.K_SPACE]: self.pos += v(0, 0, -1)*dt*speed
+        if keys[pygame.K_c]: self.pos += v(0, 0, 1)*dt*speed
+        
+        glViewport(0, 0, self.display.get_width(), self.display.get_height())
+        
+        glClearColor(0, 0, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        perspective(self.fovy, self.display.get_width()/self.display.get_height(), 0.1)
+        
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        # rotates into the FRD coordinate system
+        glMultMatrixf([
+            [ 0., 0.,-1., 0.],
+            [ 1., 0., 0., 0.],
+            [ 0.,-1., 0., 0.],
+            [ 0., 0., 0., 1.]
+        ])
+        # after that, +x is forward, +y is right, and +z is down
+        
+        glMultMatrixf(rot_matrix.T)
+        
+        glTranslate(*-self.pos)
+        
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_COLOR_MATERIAL)
+        glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, [0, 0, 0, 1])
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0, 0, 0, 1])
+        glLightfv(GL_LIGHT0, GL_POSITION, [math.sin(self.t/5)*100, math.cos(self.t/5)*100, -100, 1])
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0, 0, 0, 1])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [.5, .5, .5, 1])
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [.5, .5, .5])
+        
+        self.draw()
+        
+        #print 'start'
+        x = glReadPixels(0, 0, self.display.get_width(), self.display.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, outputType=None)
+        x = numpy.reshape(x, (self.display.get_height(), self.display.get_width(), 4))
+        x = x[::-1]
+        #print 'end', type(x)
+        
+        t = rospy.Time.now()
+        
+        quat = transformations.quaternion_from_matrix(numpy.array([(b,a,-c,0) for a, b, c in [-left, -local_up, forward]] + [(0,0,0,1)]).T)
+        self.tf_br.sendTransform((self.pos[1],self.pos[0],-self.pos[2]),
+                     quat,
+                     t,
+                     "/camera",
+                     "/map")
+        
+        
+        msg = Image()
+        msg.header.stamp = t
+        msg.header.frame_id = '/camera'
+        msg.height = self.display.get_height()
+        msg.width = self.display.get_width()
+        msg.encoding = 'rgba8'
+        msg.is_bigendian = 0
+        msg.step = self.display.get_width() * 4
+        msg.data = x.tostring()
+        self.image_pub.publish(msg)
+        
+        
+        
+        msg = CameraInfo()
+        msg.header.stamp = t
+        msg.header.frame_id = '/camera'
+        msg.height = self.display.get_height()
+        msg.width = self.display.get_width()
+        f = 1/math.tan(math.radians(self.fovy)/2)*self.display.get_height()/2
+        msg.P = [
+            f, 0, self.display.get_width()/2-.5, 0,
+            0, f, self.display.get_height()/2-.5, 0,
+            0, 0, 1, 0,
+        ]
+        self.info_pub.publish(msg)
+        
+        
+        pygame.display.flip()
+    
+    def draw(self):
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_CULL_FACE)
+        
+        for obj in self.objs:
+            obj.draw()
+        
+        # sun
+        with GLMatrix:
+            glTranslate(math.sin(self.t/5)*100, math.cos(self.t/5)*100, -100)
+            q = gluNewQuadric()
+            glDisable(GL_LIGHTING)
+            glColor3f(1, 1, 1)
+            gluSphere(q, 10, 20, 20)
+            glEnable(GL_LIGHTING)
+        
+        # water
+        glDisable(GL_LIGHTING)
+        
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        
+        glBegin(GL_TRIANGLE_FAN)
+        glNormal3f(0, 0, -1)
+        glColor4f(0, 0, 0.5, 0.5)
+        glVertex4f(0, 0, 0, 1)
+        glVertex4f(-1, -1, 0, 0)
+        glVertex4f(-1, +1, 0, 0)
+        glVertex4f(+1, +1, 0, 0)
+        glVertex4f(+1, -1, 0, 0)
+        glVertex4f(-1, -1, 0, 0)
+        glEnd()
+        
+        glBegin(GL_TRIANGLE_FAN)
+        glNormal3f(0, 0, 1)
+        glColor4f(0, 0, 0.5, 0.5)
+        glVertex4f(0, 0, 0, 1)
+        glVertex4f(-1, -1, 0, 0)
+        glVertex4f(+1, -1, 0, 0)
+        glVertex4f(+1, +1, 0, 0)
+        glVertex4f(-1, +1, 0, 0)
+        glVertex4f(-1, -1, 0, 0)
+        glEnd()
+        
+        glDisable(GL_BLEND)
+        
+        glEnable(GL_LIGHTING)
+        
+        # underwater color
+        if self.pos[2] > 0:
+            glPushMatrix()
+            glLoadIdentity()
+            
+            glMatrixMode(GL_PROJECTION)
+            glPushMatrix()
+            glLoadIdentity()
+            glMatrixMode(GL_MODELVIEW)
+            
+            glDisable(GL_LIGHTING)
+            glDisable(GL_DEPTH_TEST)
+            
+            glEnable(GL_BLEND)
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+            glBegin(GL_QUADS)
+            glColor4f(0, 0, 1, 0.5)
+            glVertex3f(-1, -1, 0)
+            glVertex3f(+1, -1, 0)
+            glVertex3f(+1, +1, 0)
+            glVertex3f(-1, +1, 0)
+            glEnd()
+            glDisable(GL_BLEND)
+            
+            glEnable(GL_DEPTH_TEST)
+            glEnable(GL_LIGHTING)
+            
+            glMatrixMode(GL_PROJECTION)
+            glPopMatrix()
+            glMatrixMode(GL_MODELVIEW)
+            
+            glPopMatrix()
+
+if __name__ == '__main__':
+    i = Interface()
+    i.init()
+    while True:
+        i.step()

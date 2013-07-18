70400025ac1404d1193a9e5b76924dca14600d98
Forrest Voight <forrest@forre.st>
2013-07-18 09:06:19 -0400

    auvsi_robosub: fixes to hedge

--- auvsi_robosub/src/auvsi_robosub/missions/hedge.py
+++ auvsi_robosub/src/auvsi_robosub/missions/hedge.py
@@ -8,7 +8,7 @@ def make_hedge(shared):
     sm_approach = smach.Sequence(['succeeded', 'failed', 'preempted'], 'succeeded')
     with sm_approach:
         smach.Sequence.add('DEPTH',
-                           common_states.WaypointState(shared, lambda cur: cur.depth(1)))
+                           common_states.WaypointState(shared, lambda cur: cur.depth(1.5)))
         smach.Sequence.add('APPROACH',
                            common_states.VelocityState(shared, numpy.array([.2, 0, 0])))
         smach.Sequence.add('WAIT_HEDGE',
@@ -21,9 +21,13 @@ def make_hedge(shared):
     with sm_center:
         smach.Sequence.add('CENTER_APPROACH_HEDGE',
                            legacy_vision_states.CenterApproachObjectState(shared, 'find2_forward_camera',
-                                                                          desired_scale=250))
+                                                                          desired_scale=180))
+        smach.Sequence.add('LEFT',
+                           common_states.WaypointState(shared, lambda cur: cur.turn_left_deg(90)))
         smach.Sequence.add('GO',
-                           common_states.WaypointState(shared, lambda cur: cur.forward(4)))
+                           common_states.WaypointState(shared, lambda cur: cur.right(4)))
+        smach.Sequence.add('RIGHT',
+                           common_states.WaypointState(shared, lambda cur: cur.turn_right_deg(90)))
     
     sm_pipe = smach.Sequence(['succeeded', 'failed', 'preempted'], 'succeeded')
     with sm_pipe:
@@ -37,13 +41,15 @@ def make_hedge(shared):
                            legacy_vision_states.WaitForObjectsState(shared,
                                                                     'find2_down_camera', 'pipe'),
                            transitions={'timeout': 'failed'})
+        smach.Sequence.add('STOP',
+                           common_states.WaypointState(shared, lambda cur: cur))
 
     sm = smach.StateMachine(['succeeded', 'failed', 'preempted'])
     with sm:
         smach.StateMachine.add('APPROACH', sm_approach,
                                transitions={'succeeded': 'CENTER',
                                             'failed': 'PIPE'})
-        smach.StateMachine.add('CENTER', sm_approach,
+        smach.StateMachine.add('CENTER', sm_center,
                                transitions={'succeeded': 'PIPE',
                                             'failed': 'PIPE'})
         smach.StateMachine.add('PIPE', sm_pipe,

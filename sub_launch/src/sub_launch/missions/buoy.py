792609ddf786ee2b6907127c9ea3da8b33dfe50c
Forrest Voight <forrest@forre.st>
2014-05-24 05:24:25 -0700

    sub_launch/sub_scripting: pool work

--- sub_launch/src/sub_launch/missions/buoy.py
+++ sub_launch/src/sub_launch/missions/buoy.py
@@ -9,9 +9,14 @@ import sub_scripting
 def main(nh):
     sub = yield sub_scripting.get_sub(nh)
     
+    orig_depth = -sub.pose.position[2]
+    
     yield sub.visual_approach_3d('forward', 1.5)
     
     yield sub.move.forward(1.5).go()
     yield sub.move.backward(1.5).go()
     
     yield sub.move.depth(0.5).go()
+    yield sub.move.forward(2).go()
+
+    yield sub.move.depth(orig_depth).go()
--- sub_launch/src/sub_launch/missions/recovery.py
+++ sub_launch/src/sub_launch/missions/recovery.py
@@ -10,10 +10,13 @@ def main(nh):
     sub = yield sub_scripting.get_sub(nh)
     
     sub.move.go(linear=[0.25, 0, 0])
+
+    dist = yield sub.get_dvl_range()
+
     
-    yield sub.visual_align('down', 'wreath')
+    yield sub.visual_align('down', 'wreath', dist)
     
-    orig_depth = -sub._pose.position[2] # XXX don't use private attr
+    orig_depth = -sub.pose.position[2]
     yield sub.move.down(1).go()
     yield sub.move.depth(-0.1).go()
     yield sub.move.depth(orig_depth).go()

5db5deb2d7ed91939af4c5227c46acf96b2fd82c
Eugene <elroberts56@gmail.com>
2014-06-02 18:02:24 -0400

    sub_launch: Tweaked the shooter.py file

---
+++ sub_launch/src/sub_launch/missions/shooter.py
@@ -0,0 +1,36 @@
+from __future__ import division
+
+from txros import util
+import numpy
+import sub_scripting
+
+def select_by_body_direction(body_vector):
+    body_vector = numpy.array(body_vector)
+    def _(results, body_tf):
+        def get_wantedness(result):
+            pos_vec = numpy.array(map(float, result['center']))
+            pos_vec_body = body_tf.transform_vector(pos_vec)
+            return pos_vec_body.dot(body_vector)
+
+        return max(results, key=get_wantedness)
+    return _
+
+@util.cancellableInlineCallbacks
+def main(nh):
+    sub = yield sub_scripting.get_sub(nh)
+    
+    sub.move.go(linear=[0.25, 0, 0])
+    
+    yield sub.visual_approach('forward', 'shooter', size_estimate=7*.0254, desired_distance=1.5, selector=select_by_body_direction([0,1,1]))
+    yield sub.move.forward(.5).go()
+    yield sub.move.backward(1.5).go()
+
+    yield sub.visual_approach('forward', 'shooter', size_estimate=7*.0254, desired_distance=1.5, selector=select_by_body_direction([0,-1,1]))
+    yield sub.move.forward(.5).go()
+    yield sub.move.backward(.5).go()
+    """yield sub.move.right(.6).go()
+    yield sub.move.forward(3.5).go()
+    yield sub.move.left(1.2).go()
+    yield sub.move.backward(3.5).go()
+    yield sub.move.right(1.2).go()
+    yield sub.move.forward(5).go()"""

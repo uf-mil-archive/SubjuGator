20dd676050e0220925e531625a7cc0a5334aed1a
Forrest Voight <forrest@forre.st>
2014-06-17 12:08:18 -0400

    sub_launch: pool mission work

--- sub_launch/src/sub_launch/missions/path.py
+++ sub_launch/src/sub_launch/missions/path.py
@@ -1,22 +1,9 @@
 from __future__ import division
 
-import numpy
-
 from txros import util
 
 import sub_scripting
 
-
-def select_by_body_direction(body_vector):
-    body_vector = numpy.array(body_vector)
-    def _(results, body_tf):
-        def get_wantedness(result):
-            pos_vec = numpy.array(map(float, result['center']))
-            pos_vec_body = body_tf.transform_vector(pos_vec)
-            return pos_vec_body.dot(body_vector)
-
-        return max(results, key=get_wantedness)
-    return _
 
 @util.cancellableInlineCallbacks
 def main(nh, direction=None):
@@ -29,8 +16,8 @@ def main(nh, direction=None):
     fwd_move = sub.move.go(linear=[0.25, 0, 0])
     try:
         yield sub.visual_align('down', 'pipe', dist, selector={
-            'left' : select_by_body_direction([0, +1, 0]),
-            'right': select_by_body_direction([0, -1, 0]),
+            'left' : sub_scripting.select_by_body_direction([0, +1, 0]),
+            'right': sub_scripting.select_by_body_direction([0, -1, 0]),
             None   : lambda objs: objs[0],
         }[direction])
     finally:

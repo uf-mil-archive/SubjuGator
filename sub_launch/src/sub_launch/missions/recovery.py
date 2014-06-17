7767ee53fb01a9b85524ac8056a9f2c5f23ba49a
Chan Yang <chanyang1993@gmail.com>
2014-06-17 09:03:26 -0400

    sub_launch added impaler support to recovery mission

--- sub_launch/src/sub_launch/missions/recovery.py
+++ sub_launch/src/sub_launch/missions/recovery.py
@@ -12,12 +12,31 @@ def main(nh):
     fwd_move = sub.move.go(linear=[0.25, 0, 0])
     try:
         dist = yield sub.get_dvl_range()
+    
+        orig_depth = -sub.pose.position[2]
         
-        yield sub.visual_align('down', 'wreath', dist)
+        dist = yield sub.get_dvl_range()
+        yield sub.move.down(dist-1.75).go()
+        
+        yield sub.visual_align('down', 'wreath/cheese', dist)
     finally:
         yield fwd_move.cancel()
     
-    orig_depth = -sub.pose.position[2]
+    yield sub.lower_impaler()
+    yield sub.move.relative([-.1,-.1,0])
     yield sub.move.down(1).go()
     yield sub.move.depth(-0.1).go()
+    yield sub.raise_impaler()
+    yield sub.move.depth(orig_depth).go()
+    
+    dist = yield sub.get_dvl_range()
+    yield sub.move.down(dist-1.75).go()
+    
+    yield sub.visual_align('down', 'wreath/moonrock', dist)
+    
+    yield sub.lower_impaler()
+    yield sub.move.relative([-.1,-.1,0])
+    yield sub.move.down(1).go()
+    yield sub.move.depth(-0.1).go()
+    yield sub.raise_impaler()
     yield sub.move.depth(orig_depth).go()

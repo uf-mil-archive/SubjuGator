76692ab04355b38d48f397557de4bd85de841dd3
Matthew Griessler <griessler.matthew@live.com>
2014-06-17 10:44:51 -0400

    updated bins

--- sub_launch/src/sub_launch/missions/bins.py
+++ sub_launch/src/sub_launch/missions/bins.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from txros import util
 import sub_scripting
+import math
 #"Landing Site"
 #the goal of this mission
 #there are four bins. Bins 1 - 3 have options of two pictures, Bin 4 is always the same picture
@@ -8,13 +9,15 @@ import sub_scripting
 #the most points are awarded for dropping in primary and secondary, dropping in any gives partial
 
 def selectPrimary(results, body_tf):
-  print results
+  #print results
   for result in results:
-    print result['image_text']
-    if result['image_text'] == '02a':
+    #print result['image_text']
+    if 'image_text' in result and result['image_text'] == '02a':
       return result
+    else:
+      return select_centered(results, body_tf)
   
-
+select_centered = lambda objs, body_tf: min(objs, key=lambda obj: math.sqrt(float(obj['center'][0])**2 + float(obj['center'][1])**2))
 
 @util.cancellableInlineCallbacks
 def main(nh):

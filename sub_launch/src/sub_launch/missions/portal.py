6143621877b3673eb67c2d42edab702ac3318523
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2015-07-14 03:36:41 -0400

    MISSIONS: Orient twice for better reliability

--- sub_launch/src/sub_launch/missions/portal.py
+++ sub_launch/src/sub_launch/missions/portal.py
@@ -16,20 +16,41 @@ def main(nh, sub=None):
         print "Marker at", angles
         if abs(angles.x)>10 or abs(angles.y)>10:
             if angles.x<0:
-                yield sub.move.left(abs(angles.x/1000)).go()
+                yield sub.move.left(abs(angles.x/500)).go()
             if angles.x>0:
-                yield sub.move.right(abs(angles.x/1000)).go()
+                yield sub.move.right(abs(angles.x/500)).go()
             if angles.y>0:
-                yield sub.move.down(abs(angles.y/1000)).go()
+                yield sub.move.down(abs(angles.y/500)).go()
             if angles.y<0:
-                yield sub.move.up(abs(angles.y/1000)).go()
+                yield sub.move.up(abs(angles.y/500)).go()
         else:
             #yield sub.move.forward(1)).go()
             break
 
     print 'CENTERED ON MARKER'
     yield sub.move.forward(2).go()
-    yield sub.move.up(0.6).go()
+
+    while True:
+        
+        angles = yield sub.get_red_marker()
+        print "Marker at", angles
+        if abs(angles.x)>10 or abs(angles.y)>10:
+            if angles.x<0:
+                yield sub.move.left(abs(angles.x/500)).go()
+            if angles.x>0:
+                yield sub.move.right(abs(angles.x/500)).go()
+            if angles.y>0:
+                yield sub.move.down(abs(angles.y/500)).go()
+            if angles.y<0:
+                yield sub.move.up(abs(angles.y/500)).go()
+        else:
+            #yield sub.move.forward(1)).go()
+            break
+
+    print 'CENTERED ON MARKER'
+
+    yield sub.move.forward(2).go()
+    yield sub.move.up(0.7).go()
     yield sub.move.turn_left_deg(90).go()
     yield sub.move.right(3).go()
 

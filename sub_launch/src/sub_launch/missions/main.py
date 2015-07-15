e76b82b02076087ba7f93dd159d6c64f93e6d61c
Zach Goins <zach.a.goins@gmail.com>
2015-07-15 10:55:43 -0400

    MISSIONS: Add pose functions to missions

--- sub_launch/src/sub_launch/missions/main.py
+++ sub_launch/src/sub_launch/missions/main.py
@@ -1,54 +1,85 @@
-dfrom __future__ import division
+from __future__ import division
 
 import traceback
 import numpy
-
 from txros import util
 from twisted.internet import threads, stdio, protocol, defer
 from twisted.protocols import basic
-
 import sub_scripting
+from sub_launch.missions import green_buoy, yellow_buoy, red_buoy, torpedo, path
 
-from sub_launch.missions import shooter
-from sub_launch.missions import buoy
-from sub_launch.missions import path
-from sub_launch.missions import manipulation
-from sub_launch.missions import bins, maneuvering, recovery
+ONE_MINUTE = 60
+TOTAL_TIME = ONE_MINUTE * 20
+BUOY_TIME = ONE_MINUTE * 3
+TORPEDO_TIME = ONE_MINUTE * 2
+PATH_TIME = ONE_MINUTE / 2
+
+@util.cancellableInlineCallbacks
+def buoys(nh):
+
+    try:
+        yield util.wrap_timeout(yellow_buoy.main(nh), BUOY_TIME / 3)
+    except Exception:
+        traceback.print_exc()
+
+    try:
+        yield util.wrap_timeout(red_buoy.main(nh), BUOY_TIME / 3)
+    except Exception:
+        traceback.print_exc()
+
+    try:
+        yield util.wrap_timeout(green_buoy.main(nh), BUOY_TIME / 3)
+    except Exception:
+        traceback.print_exc()
+
+@util.cancellableInlineCallbacks
+def torpedos(nh):
+
+    try:
+        yield util.wrap_timeout(torpedo.main(nh), TORPEDO_TIME / 2)
+    except Exception:
+        traceback.print_exc()
 
 
 @util.cancellableInlineCallbacks
 def main_list(nh):
+
+    '''
+
+    Wrap individual tasks in their own timeouts so we can assign time to 
+    the task and any support moves needed that are not included in the tasks
+    mission main function
+
+    '''
+
     sub = yield sub_scripting.get_sub(nh)
     yield sub.move.depth(1).go()
-    yield sub.move.forward(15).go()
-    m = numpy.array([7.6448368633, 41.202367025499996, 0]) * .8
-    yield sub.move.look_at_rel_without_pitching(m).go()
-    yield sub.move.absolute(m).go()
 
-@util.cancellableInlineCallbacks
-def fail_list(nh):
-    sub = yield sub_scripting.get_sub(nh)
-    yield recovery.main(nh)
+    try:
+        yield util.wrap_timeout(buoys(nh), BUOY_TIME)
+    except Exception:
+        traceback.print_exc()
+
+    try:
+        yield util.wrap_timeout(path.main(nh), PATH_TIME)
+    except Exception:
+        traceback.print_exc()
+
+    try:
+        yield util.wrap_timeout(torpedos(nh), TORPEDO_TIME)
+    except Exception:
+        traceback.print_exc()
+
 
 @util.cancellableInlineCallbacks
 def main(nh):
-    #while True:
-    #    time_left_str = yield util.nonblocking_raw_input('Enter time left: (e.g. 5:40) ')
-    #    try:
-    #        m, s = time_left_str.split(':')
-    #        time_left = 60 * int(m) + int(s)
-    #    except Exception:
-    #        traceback.print_exc()
-    #    else:
-    #        break
-    
     sub = yield sub_scripting.get_sub(nh)
     
+    # WRAP ENTIRE MISSION IN TIMEOUT
     try:
-        yield util.wrap_timeout(main_list(nh), 11*60)
+        yield util.wrap_timeout(main_list(nh), TOTAL_TIME)
     except Exception:
         traceback.print_exc()
     
-    yield fail_list(nh)
 
 
